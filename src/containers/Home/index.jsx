351e9ebffe47366143e4c11da864997a3aba8738
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-05-07 22:24:19 -0300

    feat: build home page and prepared for carousel

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,17 @@
+import { Banner, Container, Content } from './styles';
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <Banner>
+        <h1>Bem-vindo!</h1>
+      </Banner>
+      <Container>
+        <Content>
+          <div>Carrosel Categorias</div>
+          <div>Carrosel Produtos</div>
+        </Content>
+      </Container>
+    </main>
   );
 }
---
+++ src/containers/Home/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import Background from '../../assets/background.svg';
+import BannerHome from '../../assets/banner-home.svg';
+
+export const Banner = styled.div`
+    background: url('${BannerHome}');
+    background-size: cover;
+    background-position: center;
+    height: 480px;
+
+    h1 {
+        font-family: 'Road Rage', sans-serif;
+        font-size: 80px;
+        color: #f4f4f4;
+        position: absolute;
+        right: 20%;
+        top: 10%;
+    }
+`;
+
+export const Container = styled.section`
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.6),
+        rgba(255, 255, 255, 0.6)
+    ),
+    url('${Background}');
+    height: 500px;
+`;
+
+export const Content = styled.div``;
