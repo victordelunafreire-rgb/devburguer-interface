593c9a03e3b5c8c674ec26802f66366c874fbd21
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-05-30 17:40:02 -0300

    feat: added table and table proprieties for the admin interface

--- src/containers/Admin/Orders/index.jsx
+++ src/containers/Admin/Orders/index.jsx
@@ -1,7 +1,63 @@
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { useEffect, useState } from 'react';
+import { api } from '../../../services/api';
+import { Row } from './row';
+
+function createData(order) {
+  return {
+    name: order.user.name,
+    orderId: order._id,
+    date: order.createdAt,
+    status: order.status,
+    products: order.products,
+  };
+}
+
 export function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    async function loadOrders() {
+      const { data } = await api.get('orders');
+
+      setOrders(data);
+
+      console.log(data);
+    }
+    loadOrders();
+  }, []);
+
+  useEffect(() => {
+    const newRows = orders.map((order) => createData(order));
+
+    setRows(newRows);
+  }, [orders]);
+
   return (
-    <div>
-      <h2>Pedidos</h2>
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label='collapsible table'>
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Pedido</TableCell>
+            <TableCell>Cliente</TableCell>
+            <TableCell>Data do Pedido</TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row._id} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
---
+++ src/containers/Admin/Orders/row.jsx
@@ -0,0 +1,94 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+export function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label='expand row'
+            size='small'
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component='th' scope='row'>
+          {row.orderId}
+        </TableCell>
+        <TableCell align='right'>{row.name}</TableCell>
+        <TableCell align='right'>{row.date}</TableCell>
+        <TableCell align='right'>{row.status}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout='auto' unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant='h6' gutterBottom component='div'>
+                Pedido
+              </Typography>
+              <Table size='small' aria-label='purchases'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Quantidade</TableCell>
+                    <TableCell>Produto</TableCell>
+                    <TableCell>Categoria</TableCell>
+                    <TableCell></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.products.map((product) => (
+                    <TableRow key={product.id}>
+                      <TableCell component='th' scope='row'>
+                        {product.id}
+                      </TableCell>
+                      <TableCell>{product.name}</TableCell>
+                      <TableCell>{product.category}</TableCell>
+                      <TableCell>
+                        <img src={product.url} alt={product.name} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    orderId: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    products: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        category: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        quantity: PropTypes.number.isRequired,
+        url: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    status: PropTypes.string.isRequired,
+  }).isRequired,
+};
