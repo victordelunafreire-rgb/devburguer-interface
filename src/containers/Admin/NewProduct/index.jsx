ff03297d236bd12f06dff967d90dcde00b71a24f
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-05-31 19:50:48 -0300

    feat: finished the the products addition page

--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -1,7 +1,138 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Image } from '@phosphor-icons/react';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import * as yup from 'yup';
+import { api } from '../../../services/api';
+import {
+  Container,
+  ErrorMessage,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  LabelUpload,
+  Select,
+  SubmitButton,
+} from './styles';
+
+const schema = yup.object({
+  name: yup.string().required('Digite o nome do produto'),
+  price: yup
+    .number()
+    .positive()
+    .required('Digite o preço do produto')
+    .typeError('Digite o preço do produto'),
+  category: yup.object().required('Escolha uma categoria'),
+  file: yup
+    .mixed()
+    .test('required', 'Escolha um arquivo para continuar', (value) => {
+      return value && value.length > 0;
+    })
+    .test('filesize', 'Carregue arquivos até 5mb', (value) => {
+      return value && value.length > 0 && value[0].size <= 30000;
+    })
+    .test('type', 'Carregue apenas imagens PNG ou JPEG', (value) => {
+      return (
+        value &&
+        value.length > 0 &&
+        (value[0].type === 'image/jpeg' || value[0].type === 'image/png')
+      );
+    }),
+});
+
 export function NewProduct() {
+  const [fileName, setFileName] = useState(null);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
+
+      setCategories(data);
+    }
+    loadCategories();
+  }, []);
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    const productFormData = new FormData();
+
+    productFormData.append('name', data.name);
+    productFormData.append('price', data.price * 100);
+    productFormData.append('category_id', data.category.id);
+    productFormData.append('file', data.file[0]);
+
+    await toast.promise(api.post('/products', productFormData), {
+      pending: 'Adicionando o produto ...',
+      success: 'Produto criado com sucesso',
+      error: 'Falha ao adicionar o produto, tente novamente',
+    });
+  };
+
   return (
-    <div>
-      <h2>Novo Produto</h2>
-    </div>
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputGroup>
+          <Label>Nome</Label>
+          <Input type='text' {...register('name')} />
+          <ErrorMessage>{errors?.name?.message}</ErrorMessage>
+        </InputGroup>
+
+        <InputGroup>
+          <Label>Preço</Label>
+          <Input type='number' {...register('price')} />
+          <ErrorMessage>{errors?.price?.message}</ErrorMessage>
+        </InputGroup>
+
+        <InputGroup>
+          <LabelUpload>
+            <Image />
+            <input
+              type='file'
+              {...register('file')}
+              accept='image/png, image/jpeg'
+              onChange={(value) => {
+                setFileName(value.target.files[0]?.name);
+                register('file').onChange(value);
+              }}
+            />
+
+            {fileName || 'Upload do Produto'}
+          </LabelUpload>
+
+          <ErrorMessage>{errors?.file?.message}</ErrorMessage>
+        </InputGroup>
+
+        <InputGroup>
+          <Label>Categoria</Label>
+          <Controller
+            name='category'
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={categories}
+                getOptionLabel={(category) => category.name}
+                getOptionValue={(category) => category.id}
+                placeHolder='Categorias'
+                menuPortalTarget={document.body}
+              />
+            )}
+          />
+
+          <ErrorMessage>{errors?.category?.message}</ErrorMessage>
+        </InputGroup>
+
+        <SubmitButton>Adicionar Produto</SubmitButton>
+      </Form>
+    </Container>
   );
 }
