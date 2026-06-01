1d1baf4c6c3a6ac288c5b67694666feadcfdfd91
victordelunafreire-rgb <victordelunafreire@gmail.com>
2026-06-01 00:24:01 -0300

    feat: build edit product page and added the feature is in offer at the new product page

--- src/containers/Admin/EditProduct/index.jsx
+++ src/containers/Admin/EditProduct/index.jsx
@@ -1,7 +1,158 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Image } from '@phosphor-icons/react';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import * as yup from 'yup';
+import { api } from '../../../services/api';
+import {
+  Container,
+  ContainerCheckBox,
+  ErrorMessage,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  LabelUpload,
+  Select,
+  SubmitButton,
+} from './styles';
+
+const schema = yup.object({
+  name: yup.string().required('Digite o nome do produto'),
+  price: yup
+    .number()
+    .positive()
+    .required('Digite o preço do produto')
+    .typeError('Digite o preço do produto'),
+  category: yup.object().required('Escolha uma categoria'),
+  offer: yup.bool(),
+});
+
 export function EditProduct() {
+  const [fileName, setFileName] = useState(null);
+  const [categories, setCategories] = useState([]);
+
+  const navigate = useNavigate();
+
+  const {
+    state: { product },
+  } = useLocation();
+
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
+
+      setCategories(data);
+    }
+    loadCategories();
+  }, []);
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    const productFormData = new FormData();
+
+    productFormData.append('name', data.name);
+    productFormData.append('price', data.price * 100);
+    productFormData.append('category_id', data.category.id);
+    productFormData.append('file', data.file[0]);
+    productFormData.append('offer', data.offer);
+
+    await toast.promise(api.put(`/products/${product.id}`, productFormData), {
+      pending: 'Editando o produto ...',
+      success: 'Produto editado com sucesso',
+      error: 'Falha ao editar o produto, tente novamente',
+    });
+
+    setTimeout(() => {
+      navigate('/admin/produtos');
+    }, 2000);
+  };
+
   return (
-    <div>
-      <h2>Editar Produto</h2>
-    </div>
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputGroup>
+          <Label>Nome</Label>
+          <Input
+            type='text'
+            {...register('name')}
+            defaultValue={product.name}
+          />
+          <ErrorMessage>{errors?.name?.message}</ErrorMessage>
+        </InputGroup>
+
+        <InputGroup>
+          <Label>Preço</Label>
+          <Input
+            type='number'
+            {...register('price')}
+            defaultValue={product.price / 100}
+          />
+          <ErrorMessage>{errors?.price?.message}</ErrorMessage>
+        </InputGroup>
+
+        <InputGroup>
+          <LabelUpload>
+            <Image />
+            <input
+              type='file'
+              {...register('file')}
+              accept='image/png, image/jpeg'
+              onChange={(value) => {
+                setFileName(value.target.files[0]?.name);
+                register('file').onChange(value);
+              }}
+            />
+
+            {fileName || 'Upload do Produto'}
+          </LabelUpload>
+
+          <ErrorMessage>{errors?.file?.message}</ErrorMessage>
+        </InputGroup>
+
+        <InputGroup>
+          <Label>Categoria</Label>
+          <Controller
+            name='category'
+            control={control}
+            defaultValue={product.category}
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={categories}
+                getOptionLabel={(category) => category.name}
+                getOptionValue={(category) => category.id}
+                placeHolder='Categorias'
+                menuPortalTarget={document.body}
+                defaultValue={product.category}
+              />
+            )}
+          />
+
+          <ErrorMessage>{errors?.category?.message}</ErrorMessage>
+        </InputGroup>
+
+        <InputGroup>
+          <ContainerCheckBox>
+            <input
+              type='checkbox'
+              defaultChecked={product.offer}
+              {...register('offer')}
+            />
+            <Label>Produto em Oferta?</Label>
+          </ContainerCheckBox>
+        </InputGroup>
+
+        <SubmitButton>Editar Produto</SubmitButton>
+      </Form>
+    </Container>
   );
 }
--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -2,11 +2,13 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { Image } from '@phosphor-icons/react';
 import { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import * as yup from 'yup';
 import { api } from '../../../services/api';
 import {
   Container,
+  ContainerCheckBox,
   ErrorMessage,
   Form,
   Input,
@@ -25,6 +27,7 @@ const schema = yup.object({
     .required('Digite o preço do produto')
     .typeError('Digite o preço do produto'),
   category: yup.object().required('Escolha uma categoria'),
+  offer: yup.bool(),
   file: yup
     .mixed()
     .test('required', 'Escolha um arquivo para continuar', (value) => {
@@ -45,6 +48,8 @@ const schema = yup.object({
 export function NewProduct() {
   const [fileName, setFileName] = useState(null);
   const [categories, setCategories] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function loadCategories() {
@@ -69,12 +74,17 @@ export function NewProduct() {
     productFormData.append('price', data.price * 100);
     productFormData.append('category_id', data.category.id);
     productFormData.append('file', data.file[0]);
+    productFormData.append('offer', data.offer);
 
     await toast.promise(api.post('/products', productFormData), {
       pending: 'Adicionando o produto ...',
       success: 'Produto criado com sucesso',
       error: 'Falha ao adicionar o produto, tente novamente',
     });
+
+    setTimeout(() => {
+      navigate('/admin/produtos');
+    }, 2000);
   };
 
   return (
@@ -131,6 +141,12 @@ export function NewProduct() {
           <ErrorMessage>{errors?.category?.message}</ErrorMessage>
         </InputGroup>
 
+        <InputGroup>
+          <ContainerCheckBox>
+            <input type='checkbox' {...register('offer')} />
+            <Label>Produto em Oferta?</Label>
+          </ContainerCheckBox>
+        </InputGroup>
         <SubmitButton>Adicionar Produto</SubmitButton>
       </Form>
     </Container>
